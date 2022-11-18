65ba5ac43f1a778725921bbc7c80a5c902b0e9c7
AbdulHameed Olaosebikan <abdulhameed.olaosebikan@ontariotechu.net>
2022-11-18 01:35:37 -0500

    First Commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = 
 {
-    URI: "mongodb+srv://hamdo1236:password@cluster0.x7avuok.mongodb.net/workoutApp"
+    URI: "mongodb+srv://hamdo1236:hamdo1236@cluster0.x7avuok.mongodb.net/workoutApp"
 }
